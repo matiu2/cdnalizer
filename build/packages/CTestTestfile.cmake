f2ae92f040991aa548a35acc3a1548e5313ca985
Matthew Sherborne <msherborne@gmail.com>
2014-04-13 01:27:54 +1000

    Working towards getting Ubuntu packages working

---
+++ build/packages/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/matiu/projects/cdnalizer/packages
+# Build directory: /home/matiu/projects/cdnalizer/build/packages
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(debian)
