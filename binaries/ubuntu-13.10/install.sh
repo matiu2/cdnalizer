7395df621fecdec6b16c36e5d5b653a32debe284
Matthew Sherborne <msherborne@gmail.com>
2014-04-14 01:22:18 +1000

    0.1 Binary Release for Ubuntu 13.10 and up

--- binaries/ubuntu-13.10/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl https://raw.github.com/matiu2/cdnalizer/master/binaries/ubuntu-13.10/mod_cdnalizer.so > /usr/lib/apache2/modules/mod_cdnalizer.so
-curl https://raw.github.com/matiu2/cdnalizer/master/binaries/ubuntu-13.10/mod_cdnalizer.load > /etc/apache2/mods-available/mod_cdnalizer.load
-a2enmod mod_cdnalizer
