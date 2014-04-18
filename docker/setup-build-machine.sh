1c929d47103aab819363132ddfa4557927c83577
Matthew Sherborne <msherborne@gmail.com>
2014-04-19 05:47:51 +1000

    Closer to working build machine

--- docker/setup-build-machine.sh
+++ docker/setup-build-machine.sh
@@ -3,14 +3,14 @@ apt-get update
 apt-get install docker.io
 alias d=docker.io
 # Infrastructure
-d build -t postgres postgres
-d build -t pg-init postgres-init
-d build -t cdash apache-cdash
+d build -t ipostgres postgres
+d build -t ipg-init postgres-init
+d build -t icdash apache-cdash
 # Builders
 d build -t build-centos build-centos
 d build -t build-ubuntu-precise build-ubuntu-precise
 # Setup
-d run -d -name=postgres postgres
-d run -name=postgres-init --link=postgres:db postgres-init
-d run -p 80:80 -d -name=cdash --link=postgres:db cdash
+d run -d --name=postgres ipostgres
+d run --name=postgres-init --link=postgres:db ipostgres-init
+d run -p 80:80 -d --name=cdash --link=postgres:db icdash
 # TODO: git pulling machine with shared volumes
