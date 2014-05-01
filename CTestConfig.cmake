f8a3e65b732607f48dafa07f0dd102895b707180
Matthew Sherborne <msherborne@gmail.com>
2014-05-02 05:09:05 +1000

    Updated install target location for mod_cdnalizer.so

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,8 +1,7 @@
 set(CTEST_PROJECT_NAME "CDNalizer")
 
+set(CTEST_USE_LAUNCHERS 1)
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "162.242.225.50")
-#set(CTEST_DROP_SITE "www.cdash.org")
-#set(CTEST_DROP_LOCATION "/CDash/index.php?project=PublicDashboard")
 set(CTEST_DROP_LOCATION "/submit.php?project=CDNalizer")
 set(CTEST_DROP_SITE_CDASH TRUE)
