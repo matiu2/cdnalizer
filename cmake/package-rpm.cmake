3bceb5d1676fc4524ba22ba5739ecc27888e84cc
Matthew Sherborne <msherborne@gmail.com>
2014-05-05 20:44:23 +1000

    Updated RPM file after testing on centos

--- cmake/package-rpm.cmake
+++ cmake/package-rpm.cmake
@@ -2,11 +2,14 @@ SET(CPACK_RPM_PACKAGE_REQUIRES "libstdc++ >= 4.8, httpd >= 2.4")
 SET(CPACK_RPM_PACKAGE_URL "https://github.com/matiu2/cdnalizer")
 SET(CPACK_RPM_PACKAGE_LICENSE "ASL 2.0")
 
-set(YUM_DIR packages/redhat)
+# Copy postinst and postrm scripts
+set(RPM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/packages/redhat)
+configure_file(${RPM_DIR}/postinst postinst)
+file(COPY ${RPM_DIR}/01-cdnalizer.conf ${RPM_DIR}/postrm DESTINATION .)
 
-file(COPY ${YUM_DIR}/01-cdnalizer.conf ${YUM_DIR}/postrm DESTINATION .)
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/01-cdnalizer.conf DESTINATION /etc/httpd/conf.modules.d/)
+# Get our conf file that loads the module
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/01-cdnalizer.conf DESTINATION /etc/httpd/conf.d/)
 
-configure_file(${YUM_DIR}/postinst postinst)
+# Use postinst and postrm scripts
 SET(CPACK_RPM_POST_INSTALL_SCRIPT_FILE ${CMAKE_CURRENT_BINARY_DIR}/postinst )
 SET(CPACK_RPM_POST_UNINSTALL_SCRIPT_FILE ${CMAKE_CURRENT_BINARY_DIR}/postrm )
