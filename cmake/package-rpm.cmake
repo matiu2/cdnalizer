d45a6a374d530d86b0d76befac145d778a163268
Matthew Sherborne <msherborne@gmail.com>
2014-05-05 10:05:29 +1000

    Enabled creation of proper RPMs

--- cmake/package-rpm.cmake
+++ cmake/package-rpm.cmake
@@ -6,9 +6,9 @@ SET(CPACK_RPM_PACKAGE_LICENSE "ASL 2.0")
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/01-cdnalizer.conf DESTINATION /etc/httpd/conf.d/)
 
 # Copy postinst and postrm scripts
-set(YUM_DIR packages/redhat)
-configure_file(${YUM_DIR}/postinst postinst)
-file(COPY ${YUM_DIR}/01-cdnalizer.conf ${YUM_DIR}/postrm DESTINATION .)
+set(RPM packages/redhat)
+configure_file(${RPM_DIR}/postinst postinst)
+file(COPY ${RPM_DIR}/01-cdnalizer.conf ${RPM_DIR}/postrm DESTINATION .)
 
 # Use postinst and postrm scripts
 SET(CPACK_RPM_POST_INSTALL_SCRIPT_FILE ${CMAKE_CURRENT_BINARY_DIR}/postinst )
