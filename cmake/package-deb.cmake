c0c0f4157630c8aff02c747d73dcc8e523ea906e
Matthew Sherborne <msherborne@gmail.com>
2014-05-12 09:03:53 +1000

    New build for ubuntu and debian, making sure that our Apache module is loaded after mod_deflate.load

--- cmake/package-deb.cmake
+++ cmake/package-deb.cmake
@@ -4,9 +4,9 @@ SET(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://github.com/matiu2/cdnalizer")
 
 set(DEB_DIR packages/debian)
 
-configure_file(${DEB_DIR}/cdnalizer.load cdnalizer.load)
-file(COPY ${DEB_DIR}/cdnalizer.conf ${DEB_DIR}/prerm DESTINATION .)
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/cdnalizer.load ${CMAKE_CURRENT_BINARY_DIR}/cdnalizer.conf DESTINATION /etc/apache2/mods-available/)
+configure_file(${DEB_DIR}/mod_cdnalizer.load mod_cdnalizer.load)
+file(COPY ${DEB_DIR}/mod_cdnalizer.conf ${DEB_DIR}/prerm DESTINATION .)
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/mod_cdnalizer.load ${CMAKE_CURRENT_BINARY_DIR}/mod_cdnalizer.conf DESTINATION /etc/apache2/mods-available/)
 
 configure_file(${DEB_DIR}/postinst postinst)
 SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA ${CMAKE_CURRENT_BINARY_DIR}/postinst;${CMAKE_CURRENT_BINARY_DIR}/prerm )
