c29acb65aa909dae335542dbf0d0c210fa411ca9
Matthew Sherborne <msherborne@gmail.com>
2017-03-17 10:13:55 +1000

    Better docker image names

--- docker/setup-build-machine.sh
+++ docker/setup-build-machine.sh
@@ -3,14 +3,14 @@
 #apt-get install docker.io
 alias d=docker
 # Infrastructure
-docker build -t matiu/postgres postgres
-docker build -t cdnalizer/postgres-cdash postgres-cdash
-docker build -t cdnalizer/cdash apache-cdash
+docker build --name matiu/postgres postgres
+docker build --name cdnalizer/postgres-cdash postgres-cdash
+docker build --name cdnalizer/cdash apache-cdash
 # Builders
-docker build -t cdnalizer/build-centos build-centos
-docker build -t cdnalizer/build-fedora build-fedora
-docker build -t cdnalizer/build-ubuntu-precise build-ubuntu-precise
-docker build -t cdnalizer/build-ubuntu-trusty build-ubuntu-trusty
+docker build --name cdnalizer/build-centos build-centos
+docker build --name cdnalizer/build-fedora build-fedora
+docker build --name cdnalizer/build-ubuntu-precise build-ubuntu-precise
+docker build --namet cdnalizer/build-ubuntu-trusty build-ubuntu-trusty
 # Setup
 docker run -d --name=postgres cdnalizer/postgres-cdash
 docker run -p 80:80 -d --name=cdash --link=postgres:db cdnalizer/cdash
