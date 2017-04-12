a7f811f890a2cc77ea377f38749c48ea5ddf0aa5
Matthew Sherborne <msherborne@gmail.com>
2017-04-12 10:25:03 +0000

    Iterator should now support random access (except you can't go back a block; only forwards)

--- src/apache/AbstractBlockIterator.hpp
+++ src/apache/AbstractBlockIterator.hpp
@@ -28,6 +28,7 @@ struct AbstractBlockIterator
   using type = AbstractBlockIterator<SubIterator, Block>;
   using value_type = typename std::iterator_traits<SubIterator>::value_type;
   using reference = typename std::iterator_traits<SubIterator>::reference;
+  using difference_type = typename std::iterator_traits<SubIterator>::difference_type;
 
   Block block;
   SubIterator position;
@@ -55,6 +56,74 @@ struct AbstractBlockIterator
     operator++(*this);
     return result;
   }
+  // Random iterator implementation
+  type &operator+=(long count) {
+    // Assuming base iterator is random access
+    if (count > 0) {
+      while (count != 0) {
+        // How far til the end of the block ?
+        long eob = block.end() - position;
+        if (eob > count) {
+          position += count;
+          break;
+        } else {
+          // If our target is some blocks away, go to the next block and keep
+          // searching
+          count -= eob;
+          ++block;
+          position = block.begin();
+        }
+      }
+      return *this;
+    } else {
+      return *this -= -count;
+    }
+  }
+  type &operator-=(long count) {
+    if (count > 0) {
+      // How far til the beginning of the block
+      long bob = position - block.begin();
+      while (count != 0) {
+        if (bob > count) {
+          // If the target is inside this block, just move there
+          position += -count;
+          break;
+        } else {
+          // If we need to back before this block, blow up, because we don't
+          // support that
+          std::runtime_error("Can't go back to a previous block. Apache has "
+                             "already sent it to the user");
+        }
+      }
+    }
+    return *this;
+  }
+  type operator+(long count) {
+    type result(*this);
+    return result += count;
+  }
+  type operator-(long count) {
+    type result(*this);
+    return result -= count;
+  }
+  difference_type operator-(const type& other) {
+    if (block == other.block) {
+      return position - other.position;
+    } else {
+      // Assume that other is less than ours
+      auto copy = other;
+      long count = 0;
+      while (copy.block != block) {
+        count += copy.block.end() - copy.position;
+        ++copy.block;
+        copy.position = copy.block.begin();
+        if (copy.isSentinel())
+          throw std::runtime_error("Couldn't find a distance between two iterators");
+      }
+      return count + (position - other.position);
+    }
+  }
+  
   bool operator==(const type &other) const {
     if (block.isSentinel() && other.block.isSentinel())
       return true;
