1f555b8a656d97458452b555e4fb581b3c80b6ba
Matthew Sherborne <msherborne@gmail.com>
2017-05-25 18:53:49 +0000

    skipOverCount unused variable removed

--- src/path.hpp
+++ src/path.hpp
@@ -7,26 +7,20 @@
 namespace cdnalizer {
 
 /// Takes a relative path and returns a canonical URL in a string.
-/// Furthermore it updates `skipOverCount`, which is later used
-/// to skip over original text that won't be output in the final text
 ///
-/// @param skipOverCount will be filled with how many letters to skip over
 /// @param location Our path location according to Apache
 /// @param path The path as presented by the original text
 /// @tparam Path path can be any sequence of chars, either a
 ///         boost::iterator_range or a std::string will be fine
 template <typename Path>
-std::string canonicalFromRelativePath(int &skipOverCount,
-                                      const std::string &location,
+std::string canonicalFromRelativePath(const std::string &location,
                                       const Path &path) {
   // The canonical path will start with location
   std::string canonical(location);
   // If it doesn't end in a '/' add one and remember that the slash is not in
   // the original path
-  if (location.back() != '/') {
+  if (location.back() != '/')
     canonical.push_back('/');
-    --skipOverCount;
-  }
   std::copy(path.begin(), path.end(),
             std::back_inserter(canonical));
   // The written url will be 'images/x', but canonical will be
@@ -34,16 +28,11 @@ std::string canonicalFromRelativePath(int &skipOverCount,
   // later we'll find that '/blog/images/' is in the config, so skip over 12
   // chars (len("/blog/image")) but we only want to skip over 6 chars ('images/')
   // so we take away len('/blog') which is our location + 2 for the slash
-  skipOverCount -= location.size();
   return canonical;
 }
 
 /// Takes an absolute path and returns a canonical URL in a string.
-/// It updates `skipOverCount`, which is later used to skip over original text
-/// that won't be output in the final text
 ///
-/// @param skipOverCount will be incremented and/or decremented with how many
-///        letters to skip over
 /// @param location Our path location according to Apache
 /// @param path The path as presented by the original text
 /// @param server_url The protocol and port of the server, eg. https://supa.ws
@@ -51,7 +40,7 @@ std::string canonicalFromRelativePath(int &skipOverCount,
 ///         boost::iterator_range or a std::string will be fine
 template <typename Path>
 std::string
-canonicalFromAbsolutePath(int &skipOverCount, const std::string &location,
+canonicalFromAbsolutePath(const std::string &location,
                           const Path &path, const std::string &server_url) {
   if (!server_url.empty()) {
     // TODO: In reality there may be several server_url aliases; we should
@@ -59,9 +48,6 @@ canonicalFromAbsolutePath(int &skipOverCount, const std::string &location,
     auto match = utils::mismatch(server_url.cbegin(), server_url.cend(),
                                  path.begin(), path.end());
     if (match.first == server_url.cend()) {
-      // If the path in the source text contains the server_url,
-      // we want to skip over that extra length in the source
-      skipOverCount += server_url.length();
       // We'll return the part after the hostname
       return std::string(match.second, path.end());
     }
@@ -71,18 +57,20 @@ canonicalFromAbsolutePath(int &skipOverCount, const std::string &location,
   return std::string(path.begin(), path.end());
 }
 
+const std::string empty;
+
 using FindCDNURL = std::function<const std::pair<std::string, std::string> &(
                     const std::string &)>;
 
 template <typename Path>
 std::pair<int, const std::string &>
-handlePath(int &skipOverCount, const std::string &location, const Path &path,
+handlePath(const std::string &location, const Path &path,
            const std::string &server_url, FindCDNURL findCDNUrl) {
   // First get the canonical version of the string
   std::string canonical(
-      utils::is_relative(start, end)
-          ? canonicalFromRelativePath(skipOverCount, location, path)
-          : canonicalFromAbsolutePath(skipOverCount, location, path,
+      utils::is_relative(path.begin(), path.end())
+          ? canonicalFromRelativePath(location, path)
+          : canonicalFromAbsolutePath(location, path,
                                       server_url));
 
   // Now we know what to search for in our map
@@ -95,10 +83,8 @@ handlePath(int &skipOverCount, const std::string &location, const Path &path,
   auto found(findCDNUrl(canonical));
   if (found.first.empty() && found.second.empty()) {
     // We found nothing
-    return {{}, 0, empty};
+    return {0, empty};
   }
-
-  skipOverCount += found.first.size();
 
   const std::string &base_path = found.first;
   const std::string &cdn_url = found.second;
